dea793391c8d28edc2488aac1646fa7ff40a7e09
George Andronache <georgeandronache.cpp@gmail.com>
2023-10-24 23:04:03 +0200

    integrated Get Currently Playing track to /spotify command upon authorization, TODO - automate the process in a separate goroutine when song reaches end or user does external interaction

--- handler/commands/spotify_command.go
+++ handler/commands/spotify_command.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"fmt"
+	"encoding/json"
+	"net/http"
 
 	"github.com/georgecpp/mimir/misc"
 	"github.com/slack-go/slack"
@@ -28,19 +30,44 @@ func HandleSpotifyCommand(command slack.SlashCommand, client *slack.Client) (int
 		return nil, nil
 	}
 
-	// Define your dummy data
-	artist := "Dummy Artist"
-	song := "Dummy Song"
-	imageURL := "https://i.scdn.co/image/ab67616d0000b273a63fc9073db1233ea6c7ae74"
+	// Make a GET request to Spotify API
+	url := "https://api.spotify.com/v1/me/player/currently-playing"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+
+	httpClient := &http.Client{}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected response: %s", resp.Status)
+	}
+
+	var data map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	// Extract the information from 'data'
+	artist := data["item"].(map[string]interface{})["artists"].([]interface{})[0].(map[string]interface{})["name"].(string)
+	song := data["item"].(map[string]interface{})["name"].(string)
+	imageURL := data["item"].(map[string]interface{})["album"].(map[string]interface{})["images"].([]interface{})[1].(map[string]interface{})["url"].(string)
 
 	// Create the image block
-	imageBlock := slack.NewImageBlockElement(imageURL, "Album Cover")
+	albumImageBlock := slack.NewImageBlock(imageURL, "Album Cover", "", nil)
 
 	// Create the text block with artist and song details
 	textBlock := slack.NewTextBlockObject(slack.MarkdownType, fmt.Sprintf("*Artist:* %s\n*Song:* %s", artist, song), false, false)
 
 	// Create the section block with the text and image blocks
-	sectionBlock := slack.NewSectionBlock(textBlock, nil, slack.NewAccessory(imageBlock))
+	songMetadataBlock := slack.NewSectionBlock(textBlock, nil, nil)
 
 	// Create buttons for controls
 	previousButton := slack.NewButtonBlockElement("", "skip_previous", slack.NewTextBlockObject(slack.PlainTextType, "⏪", false, false))
@@ -59,14 +86,15 @@ func HandleSpotifyCommand(command slack.SlashCommand, client *slack.Client) (int
 	attachment := slack.Attachment{
 		Blocks: slack.Blocks{
 			BlockSet: []slack.Block{
-				sectionBlock,
+				albumImageBlock,
+				songMetadataBlock,
 				actionBlock,
 			},
 		},
 	}
 
 	 // Post the message to the channel
-	 _, _, err := client.PostMessage(command.ChannelID, slack.MsgOptionAttachments(attachment))
+	 _, _, err = client.PostMessage(command.ChannelID, slack.MsgOptionAttachments(attachment))
 	 if err != nil {
 		 return nil, fmt.Errorf("failed to post message: %w", err)
 	 }
